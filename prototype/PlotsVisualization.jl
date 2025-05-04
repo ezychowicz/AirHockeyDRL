b9be8c91a9f3b54cb3daa4b534d7df261447fa2d
Emil <emil.zychowicz@gmail.com>
2025-05-04 21:09:21 +0200

    okej juz cos nie cos dziala

--- prototype/PlotsVisualization.jl
+++ prototype/PlotsVisualization.jl
@@ -1,10 +1,15 @@
-push!(LOAD_PATH, joinpath(@__DIR__, "..", "src")) # zeby dzialalo jakby bylo w src
-using Plots, Environment, CoreTypes
-gr()  # backend do animacji
- 
-using Environment
+# Dodaj ścieżkę do źródeł, jeśli jest potrzebne
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+
+# Importujemy potrzebne moduły
+using Plots
+using AirHockey  
+include("TracedEnv.jl")
+using .TracedEnv
+gr()  # Ustawienie backendu dla animacji
+
 # === INICJALIZACJA === #
-params = Environment.EnvParams(
+params = AirHockey.EnvParams(  
     x_len = 100.0f0,
     y_len = 50.0f0,
     goal_width = 20.0f0,
@@ -12,7 +17,7 @@ params = Environment.EnvParams(
     mallet_radius = 2.0f0,
     agent1_initial_pos = [10.0f0, 25.0f0],
     agent2_initial_pos = [90.0f0, 25.0f0],
-    dt = 0.2f0,
+    dt = 0.02f0,
     terminal_velo = 1.0f0,
     band_e_loss = 0.05f0,
     restitution = 0.95f0,
@@ -20,25 +25,33 @@ params = Environment.EnvParams(
     mallet_mass = 2.0f0
 )
 
-env = Environment.AirHockeyEnv(params, Environment.State(nothing, nothing, Environment.Puck([0.0f0, 0.0f0], [0.0f0, 0.0f0])), false)
-Environment.reset!(env)
+# Tworzymy instancję środowiska z AirHockeyEnv
+env = AirHockey.AirHockeyEnv(params, AirHockey.State(nothing, nothing, AirHockey.Puck([0.0f0, 0.0f0], [0.0f0, 0.0f0])), false)
 
+# Resetujemy środowisko
+AirHockey.reset!(env)
 
+# Ustawiamy początkową prędkość krążka
 env.state.puck.v .= [60.0f0, 15.0f0]
 
 # === ANIMACJA === #
-frames = @gif for i in 1:60
-    Environment.simulate_dt!(env)
+frames = @gif for i in 1:50
+    trace = TracedEnv.simulate_dt!(env)  
+
+    puck_positions = trace.puck_trace
+    println(puck_positions)
+    xs, ys = map(x -> x[1], puck_positions), map(x -> x[2], puck_positions)
     
-    puck_pos = env.state.puck.pos
+    println(xs,ys)
     scatter(
-        [puck_pos[1]], [puck_pos[2]];
+        xs, ys; 
         xlim=(0, params.x_len), ylim=(0, params.y_len),
         legend=false, xlabel="x", ylabel="y",
         title="Klatka $i",
         markersize=8, markercolor=:red
     )
 
-    # opcjonalnie: zaznacz bandy, bramki, itp.
+    # opcjonalnie: dodaj inne elementy jak bandy, bramki itp.
 end every 1
+
 
---
+++ prototype/TracedEnv.jl
@@ -0,0 +1,56 @@
+# stworzone aby nie ingerować w kod RLowy ale żeby zapisywać stany potrzebne do animacji
+
+module TracedEnv
+using AirHockey # Puck itd. są eksportowane wiec nie musze przedrostków dodawać 
+mutable struct Trace{T<:Real}
+    puck_trace::Vector{Vector{T}}
+    mallet1_trace::Vector{Vector{T}}
+    mallet2_trace::Vector{Vector{T}}
+end
+function update_positions!(
+    puck::Puck,
+    mallet1::Union{Mallet, Nothing},
+    mallet2::Union{Mallet, Nothing},
+    dt::T,
+    trace::Trace{V}
+) where {V,T<:Real}
+    
+    puck.pos += puck.v * dt
+    if mallet1 !== nothing
+        mallet1.pos += mallet1.v * dt
+    end
+    if mallet2 !== nothing
+        mallet2.pos += mallet2.v * dt
+    end
+
+    push!(trace.puck_trace, copy(puck.pos))
+    push!(trace.mallet1_trace, mallet1 !== nothing ? copy(mallet1.pos) : Vector{V}())
+    push!(trace.mallet2_trace, mallet2 !== nothing ? copy(mallet2.pos) : Vector{V}())
+end
+
+function simulate_dt!(env::AirHockeyEnv)
+    mallet1, mallet2, puck = env.state.agent1, env.state.agent2, env.state.puck
+    t_remain = env.params.dt
+    trace = Trace{Float32}(Vector{Vector{Float32}}(), Vector{Vector{Float32}}(),Vector{Vector{Float32}}())
+    while t_remain > 0
+        tx, ty = AirHockey.time_to_wall(env, puck)
+        tm1 = AirHockey.time_to_mallet(env, puck, mallet1) 
+        tm2 = AirHockey.time_to_mallet(env, puck, mallet2)
+        tm1_to_wallx, tm1_to_wally = AirHockey.time_to_wall(env, mallet1)
+        tm2_to_wallx, tm2_to_wally = AirHockey.time_to_wall(env, mallet2)
+        # Znajdź najbliższe zdarzenie - UWAGA: tak naprawdę tylko ten pierwszy czas jest zawsze prawdziwy.
+        # Obliczone czasy nie uwzględniają  bowiem zderzenia, które się wydarzy w minimum z tych czasów.
+        times = [tx, ty, tm1, tm2, tm1_to_wallx, tm2_to_wallx, tm1_to_wally, tm2_to_wally]
+        t_next, idx = findmin(times)
+
+        update_positions!(puck, mallet1, mallet2, t_next, trace) # przewiń symulacje do momentu kolizji
+
+        if t_next != Inf # doszło do kolizji
+            AirHockey.execute_collision!(env, idx)
+        end
+        t_remain -= t_next # zmniejsz pozostały czas o czas już zhandlowany
+    end
+    return trace
+end
+
+end
