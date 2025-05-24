7ed8ea25a578fc754adf1008ba3f33184fbd420b
Emil <emil.zychowicz@gmail.com>
2025-05-24 03:43:28 +0200

    rysowanie w miare juz dziala w Makie

--- prototype/PlotsVisualization.jl
+++ prototype/PlotsVisualization.jl
@@ -3,10 +3,12 @@ push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
 
 # Importujemy potrzebne moduły
 using Plots
+using GLMakie
+# using GeometryBasics
 using AirHockey  
 include("TracedEnv.jl")
 using .TracedEnv
-gr()  # Ustawienie backendu dla animacji
+# gr()  # Ustawienie backendu dla animacji
 
 # === INICJALIZACJA === #
 params = AirHockey.EnvParams(  
@@ -19,39 +21,104 @@ params = AirHockey.EnvParams(
     agent2_initial_pos = [90.0f0, 25.0f0],
     dt = 0.02f0,
     terminal_velo = 1.0f0,
-    band_e_loss = 0.05f0,
+    band_e_loss = 0.95f0,
     restitution = 0.95f0,
     puck_mass = 1.0f0,
     mallet_mass = 2.0f0
 )
 
 # Tworzymy instancję środowiska z AirHockeyEnv
-env = AirHockey.AirHockeyEnv(params, AirHockey.State(nothing, nothing, AirHockey.Puck([0.0f0, 0.0f0], [0.0f0, 0.0f0])), false)
+agent1 = AirHockey.Mallet([0.0f0, 0.0f0], [5.0f0, 25.0f0])
+agent2 = AirHockey.Mallet([0.0f0, 0.0f0], [95.0f0, 25.0f0])
+env = AirHockey.AirHockeyEnv(params, AirHockey.State(agent1, agent2, AirHockey.Puck([0.0f0, 0.0f0], [25.0f0, 25.0f0])), false)
 
-# Resetujemy środowisko
-AirHockey.reset!(env)
+
 
 # Ustawiamy początkową prędkość krążka
-env.state.puck.v .= [60.0f0, 15.0f0]
+env.state.puck.v .= [-5.0f0, 1.0f0]
 
 # === ANIMACJA === #
-frames = @gif for i in 1:50
-    trace = TracedEnv.simulate_dt!(env)  
+function interpolate_movement(xs::Vector{V},ys::Vector{V},vs::Vector{Vector{T}}, times::Vector{Float32}; precision::Int64 = 1000) where {V,T <: Real}
+    """
+    Interpoluj ruchy między kolejnymi stanami uzyskanymi z całej symulacji. 
+    Uzupełniaj stanami pośrednimi, zakładając r. jednostajny prostoliniowy.
+    vs niepotrzebne przy takim założeniu, ale wole zostawić.
+    """
+    xs_diffs = xs[2:end] .- xs[1:end-1]
+    new_xs, new_ys = Vector{V}(), Vector{V}()
+    full_time = sum(times) # czas calej symulacji
+    for i in eachindex(xs_diffs)
+        k = times[i]/full_time # czas kroku wzgledem danej symulacji: liczba klatek ma być proporcjonalna
+        frames = max(2,round(Int64,k*precision))
+        println("frames:$frames")
+        xs_filler = collect(range(xs[i], xs[i+1], frames))
+        ys_filler = collect(range(ys[i], ys[i+1], frames))
+        append!(new_xs, xs_filler)
+        append!(new_ys, ys_filler)
+    end
+    new_xs, new_ys
+end
 
-    puck_positions = trace.puck_trace
-    println(puck_positions)
-    xs, ys = map(x -> x[1], puck_positions), map(x -> x[2], puck_positions)
-    
-    println(xs,ys)
-    scatter(
-        xs, ys; 
-        xlim=(0, params.x_len), ylim=(0, params.y_len),
-        legend=false, xlabel="x", ylabel="y",
-        title="Klatka $i",
-        markersize=8, markercolor=:red
-    )
+function simulate()
+    puck_states = Vector{AirHockey.Puck}()
+    mallet1_states = Vector{AirHockey.Mallet}()
+    mallet2_states = Vector{AirHockey.Mallet}()
+    times_cumul = Vector{Float32}()
+    for _ ∈ 1:50
+        trace = TracedEnv.simulate_dt!(env)  
+        append!(times_cumul, trace.times)
+        append!(puck_states, trace.puck_trace)
+        append!(mallet1_states, trace.mallet1_trace)
+        append!(mallet2_states, trace.mallet2_trace)
+    end
+    puck_states, mallet1_states, mallet2_states, times_cumul
+end
 
-    # opcjonalnie: dodaj inne elementy jak bandy, bramki itp.
-end every 1
+function interpolate_simulation(puck_states, mallet1_states, mallet2_states, times)
+    puck_positions = map(puck -> puck.pos, puck_states)
+    mallet1_positions = map(mallet -> mallet.pos, mallet1_states)
+    mallet2_positions = map(mallet -> mallet.pos, mallet2_states)
+    puck_vs = map(puck -> puck.v, puck_states)
+    mallet1_vs = map(mallet -> mallet.v, mallet1_states)
+    mallet2_vs = map(mallet -> mallet.v, mallet2_states)
+    puck_xs, puck_ys = map(x -> x[1], puck_positions), map(x -> x[2], puck_positions)
+    mallet1_xs, mallet1_ys = map(x -> x[1], mallet1_positions), map(x -> x[2], mallet1_positions)
+    mallet2_xs, mallet2_ys = map(x -> x[1], mallet2_positions), map(x -> x[2], mallet2_positions)
+    new_puck_xs, new_puck_ys = interpolate_movement(puck_xs,puck_ys,puck_vs, times)
+    new_mallet1_xs, new_mallet1_ys = interpolate_movement(mallet1_xs, mallet1_ys, mallet1_vs, times)
+    new_mallet2_xs, new_mallet2_ys = interpolate_movement(mallet2_xs, mallet2_ys, mallet2_vs, times)
+    return new_puck_xs, new_puck_ys, new_mallet1_xs, new_mallet1_ys, new_mallet2_xs, new_mallet2_ys
+end
+
+function one_step_progress(i)
+    new_puck_xs[i], new_puck_ys[i], new_mallet1_xs[i], new_mallet1_ys[i], new_mallet2_xs[i], new_mallet2_ys[i]
+end 
+
+function animstep!(axis, xy_puck, xy_mallet12, i)
+    a,b,c,d,e,f = one_step_progress(i)
+    xy_puck[] = [Point2f(a,b)]
+    xy_mallet12[] = [Point2f(c,d),Point2f(e,f)] 
+    axis.title[] = "Klatka $i"
+end
 
 
+new_puck_xs, new_puck_ys, new_mallet1_xs, new_mallet1_ys, new_mallet2_xs, new_mallet2_ys = interpolate_simulation(simulate()...)
+xy_puck = Observable([Point2f(new_puck_xs[1], new_puck_ys[1])])
+xy_mallet12 = Observable([Point2f(new_mallet1_xs[1], new_mallet1_ys[1]), Point2f(new_mallet2_xs[1], new_mallet2_ys[1])])
+f = Figure()
+f = Figure(size = (600, 400),camera = campixel!)
+scene = Scene(size = (600, 400),camera = campixel!)
+axis = f[1,1] = Axis(scene;
+    limits = ((0,params.x_len), (0,params.y_len)),
+    xlabel = "x", ylabel = "y",
+    title = "Klatka 1"
+)
+
+scatter_puck = GLMakie.scatter!(axis, xy_puck; color = :red, markersize = 20env.params.puck_radius)
+scatter_mallets = GLMakie.scatter!(axis, xy_mallet12; color = :blue, markersize = 20env.params.mallet_radius)
+display(scene)
+
+for i in eachindex(new_puck_xs)
+    animstep!(axis, xy_puck, xy_mallet12, i)
+    sleep(0.01)
+end
--- prototype/TracedEnv.jl
+++ prototype/TracedEnv.jl
@@ -3,17 +3,17 @@
 module TracedEnv
 using AirHockey # Puck itd. są eksportowane wiec nie musze przedrostków dodawać 
 mutable struct Trace{T<:Real}
-    puck_trace::Vector{Vector{T}}
-    mallet1_trace::Vector{Vector{T}}
-    mallet2_trace::Vector{Vector{T}}
+    puck_trace::Vector{Puck}
+    mallet1_trace::Vector{Mallet}
+    mallet2_trace::Vector{Mallet}
+    times::Vector{T} # różnice czasów między kolejnymi stanami Tracea
 end
 function update_positions!(
     puck::Puck,
     mallet1::Union{Mallet, Nothing},
     mallet2::Union{Mallet, Nothing},
     dt::T,
-    trace::Trace{V}
-) where {V,T<:Real}
+) where {T<:Real}
     
     puck.pos += puck.v * dt
     if mallet1 !== nothing
@@ -23,15 +23,13 @@ function update_positions!(
         mallet2.pos += mallet2.v * dt
     end
 
-    push!(trace.puck_trace, copy(puck.pos))
-    push!(trace.mallet1_trace, mallet1 !== nothing ? copy(mallet1.pos) : Vector{V}())
-    push!(trace.mallet2_trace, mallet2 !== nothing ? copy(mallet2.pos) : Vector{V}())
+
 end
 
 function simulate_dt!(env::AirHockeyEnv)
     mallet1, mallet2, puck = env.state.agent1, env.state.agent2, env.state.puck
     t_remain = env.params.dt
-    trace = Trace{Float32}(Vector{Vector{Float32}}(), Vector{Vector{Float32}}(),Vector{Vector{Float32}}())
+    trace = Trace{Float32}(Vector{Puck}(), Vector{Mallet}(),Vector{Mallet}(),Vector{Float32}())
     while t_remain > 0
         tx, ty = AirHockey.time_to_wall(env, puck)
         tm1 = AirHockey.time_to_mallet(env, puck, mallet1) 
@@ -43,11 +41,15 @@ function simulate_dt!(env::AirHockeyEnv)
         times = [tx, ty, tm1, tm2, tm1_to_wallx, tm2_to_wallx, tm1_to_wally, tm2_to_wally]
         t_next, idx = findmin(times)
 
-        update_positions!(puck, mallet1, mallet2, t_next, trace) # przewiń symulacje do momentu kolizji
+        update_positions!(puck, mallet1, mallet2, t_next) # przewiń symulacje do momentu kolizji
 
         if t_next != Inf # doszło do kolizji
             AirHockey.execute_collision!(env, idx)
         end
+        push!(trace.puck_trace, deepcopy(puck))
+        push!(trace.mallet1_trace, mallet1 !== nothing ? deepcopy(mallet1) : Mallet(Vector{V}(),Vector{V}()))
+        push!(trace.mallet2_trace, mallet2 !== nothing ? deepcopy(mallet2) : Mallet(Vector{V}(),Vector{V}()))
+        push!(trace.times, t_next)
         t_remain -= t_next # zmniejsz pozostały czas o czas już zhandlowany
     end
     return trace
